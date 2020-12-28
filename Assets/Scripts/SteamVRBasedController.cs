e8341ca44192753ce91b6bfe0a799daaca460b94
mkitVR <mkiit@gmail.com>
2020-12-28 18:37:13 +0300

    last

---
+++ Assets/Scripts/SteamVRBasedController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Valve.VR;
+
+public class SteamVRBasedController : XRBaseController
+{
+
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Source.Any;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
